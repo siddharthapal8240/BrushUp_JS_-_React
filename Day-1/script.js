7d3bd0131b8620a37e6f1619c9dc93b4b0231d85
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-04 23:09:16 +0530

    Update

---
+++ Day-1/script.js
@@ -0,0 +1,21 @@
+console.log('Hello World')
+
+// var a=5;
+// var b=6;
+// var c="sid";
+
+let a=5;
+let b=6;
+let c="sid";
+
+//in moden js we need to use let instead of var
+
+// console.log(a+b+8);
+// console.log(typeof a , typeof b , typeof c);
+// const d=5;
+
+{
+    let a=66;
+    console.log(a)
+}
+console.log(a)
