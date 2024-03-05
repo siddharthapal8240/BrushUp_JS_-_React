75420ae6f31327253e1753bb2c2d39843e7857ba
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-05 15:56:57 +0530

    Update

--- Day-2/test1.js
+++ Day-2/test1.js
@@ -1,23 +1,25 @@
-// function test(name) {
-//     console.log("Hello " + name);
-//     console.log("Hello " + name + " Welcome to the world of JavaScript");
-//     console.log("Hello " + name + " Welcome to the world of coding");
+//function in js
+
+function test(name) {
+    console.log("Hello " + name);
+    console.log("Hello " + name + " Welcome to the world of JavaScript");
+    console.log("Hello " + name + " Welcome to the world of coding");
     
-// }
+}
 
-// test("John")
-// test("Doe")
+test("John")
+test("Doe")
 
 
-// function sum(a,b) {
-// console.log(a+b)
+function sum(a,b) {
+console.log(a+b)
 
-// return a+b
-// }
+return a+b
+}
 
-// result = sum(10,20)
+result = sum(10,20)
 
-// console.log("my result is: " + result)
+console.log("my result is: " + result)
 
 
 const func1 = (x) => {
