7f78fc7d6c62bbabba3bf4d1c8079cb55b6c2788
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-05 19:04:12 +0530

    update

---
+++ Day-2/strings.js
@@ -0,0 +1,30 @@
+let a ="Siddhartha"
+console.log(a)
+console.log(a[0])
+console.log(a[1])
+console.log(a[2])
+console.log(a[3])
+console.log(a[4])
+console.log(a[5])
+console.log(a[6])
+console.log(a[7])
+console.log(a[8])
+console.log(a[9])
+// console.log(a[10])
+
+console.log(a.length)
+
+let real_name ="Siddhartha"
+let friend = "Sayan"
+
+console.log(real_name + " and " + friend + " are friends")   // Siddhartha and Sayan are friends
+console.log(`${real_name} and ${friend} are friends`)   // Siddhartha and Sayan are friends
+
+
+let b = "Sid"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
+console.log(b.slice(0,2))
+
+console.log(b.replace("S","Hi"))
