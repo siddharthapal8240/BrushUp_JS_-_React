562bcf59b3b45e4099c85b2ae26e0ca3644aa461
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-06 20:15:16 +0530

    Update

---
+++ Day-3/index.js
@@ -0,0 +1,11 @@
+// let arr = [1, 2, 3, 4, 5]
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// console.log(arr[5]);
+// console.log(arr[6]);
