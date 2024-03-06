c58717ae9d656d591ca03f01c3ed76c349d18870
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-06 20:21:48 +0530

    Update

--- Day-3/index.js
+++ Day-3/index.js
@@ -1,12 +1,14 @@
 let arr = [1, 2, 3, 4, 5]
 
-console.log(arr);
-console.log(arr.length);
-console.log(arr[0]);
-console.log(arr[1]);
-console.log(arr[2]);
-console.log(arr[3]);
-console.log(arr[4]);
-arr[5] = 60;
-console.log(arr[5]);
-console.log(arr[6]);
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0]);
+// console.log(arr[1]);
+// console.log(arr[2]);
+// console.log(arr[3]);
+// console.log(arr[4]);
+// arr[5] = 60;
+// console.log(arr[5]);
+// console.log(arr[6]);
+ console.log(arr, typeof arr);
+ console.log(arr.toString())
