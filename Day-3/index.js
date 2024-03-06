f9e3fda843ad93b0951831959a4fdf41fc8e5729
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-07 00:10:34 +0530

    Update

--- Day-3/index.js
+++ Day-3/index.js
@@ -30,3 +30,55 @@
     // let a2= [4, 5, 6]
     // let a3= [7, 8, 9]
     // console.log( a1.concat(a2, a3))
+
+    // let arr = [1, 2, 3, 4, 5]
+    // arr.splice(1, 1)
+    // console.log(arr)
+
+    // let arr = [1, 2, 3, 4, 5]
+
+    // for (let i = 0; i < arr.length; i++) {
+    //     console.log(arr[i]);
+    // }
+    // arr.forEach((element, index, arr) => {
+    //     console.log(element, index, arr);
+    // });
+
+    // let obj = {
+    //     name: 'Sid',
+    //     age: 21,
+    //     city: 'Kolkata'
+    // }
+    // for (const key in obj) {
+    //     if (Object.hasOwnProperty.call(obj, key)) {
+    //         const element = obj[key];
+    //         console.log(key ,element);
+            
+    //     }
+    // }
+
+    // for (const value of arr) {
+    //     console.log(value);
+    // }
+
+    // let newarr = arr.map((element) => {
+    //     return element * 2
+    // })
+    // console.log(newarr);
+
+    // const greaterThanTwo = arr.filter((element) => {
+    //     if (element > 2) {
+    //         return true
+    //     }
+    //     return false
+    // })
+
+    // console.log(greaterThanTwo);
+
+    // const red=(a,b)=>{
+    //     return a+b
+    // }
+    // console.log(arr.reduce(red))
+
+    Array.from('Sid')
+    console.log(Array.from('Sid'));
