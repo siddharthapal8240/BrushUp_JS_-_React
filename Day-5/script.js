2847a8c66c8e6a5f72f5f634066bc75a7a2dcf51
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-10 20:12:25 +0530

    Update

--- Day-5/script.js
+++ Day-5/script.js
@@ -4,14 +4,18 @@
 //     alert('Button Clicked')
 // });
 
-document.querySelector(".child").addEventListener("click" , (e)=>{
-    e.stopPropagation()
-    alert("clicked")
-})
-document.querySelector(".childcontainer").addEventListener("click" , (e)=>{
-    alert("childcontainer was clicked")
-})
-document.querySelector(".container").addEventListener("click" , (e)=>{
-    alert("container was clicked")
-})
+// document.querySelector(".child").addEventListener("click" , (e)=>{
+//     e.stopPropagation()
+//     alert("clicked")
+// })
+// document.querySelector(".childcontainer").addEventListener("click" , (e)=>{
+//     alert("childcontainer was clicked")
+// })
+// document.querySelector(".container").addEventListener("click" , (e)=>{
+//     alert("container was clicked")
+// })
+
+// setInterval(() => {
+//     document.querySelector(".container").style.backgroundColor = `rgb(${Math.random()*255},${Math.random()*255},${Math.random()*255})`
+// }, 1000);
 
