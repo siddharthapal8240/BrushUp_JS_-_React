8254033f12b48c6787fb4e249b878683ff13711b
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-10 19:44:33 +0530

    Update

--- Day-5/script.js
+++ Day-5/script.js
@@ -1,5 +1,17 @@
-let button = document.getElementById('btn');
+// let button = document.getElementById('btn');
 
-button.addEventListener('click', () => {
-    alert('Button Clicked')
-});
+// button.addEventListener('click', () => {
+//     alert('Button Clicked')
+// });
+
+document.querySelector(".child").addEventListener("click" , (e)=>{
+    e.stopPropagation()
+    alert("clicked")
+})
+document.querySelector(".childcontainer").addEventListener("click" , (e)=>{
+    alert("childcontainer was clicked")
+})
+document.querySelector(".container").addEventListener("click" , (e)=>{
+    alert("container was clicked")
+})
+
