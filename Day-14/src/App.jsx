cd5ee02cc464d199a5741b96b112005b109e4af8
Siddhartha Pal <118398335+siddharthapal8240@users.noreply.github.com>
2024-03-17 20:33:06 +0530

    Update

---
+++ Day-14/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+     <div className="container">
+      <div className="bg-red-600">
+        Hey i am good
+      </div>
+     </div>
+    </>
+  )
+}
+
+export default App
